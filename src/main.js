e4cceee06324f912c2d7e36a943050d51760788b
hathweels <12772651+hatwheels@users.noreply.github.com>
2020-02-15 12:13:24 +0200

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+import Vuetify from 'vuetify'
 import DefaultLayout from '~/layouts/Default.vue'
 
+import 'vuetify/dist/vuetify.min.css'
+
 export default function (Vue, { router, head, isClient }) {
+  Vue.use(Vuetify)
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
