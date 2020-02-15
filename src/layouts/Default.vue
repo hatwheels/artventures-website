9ae00042337fd2ac4fa7a537c4894bea148c13c6
hathweels <12772651+hatwheels@users.noreply.github.com>
2020-02-15 21:35:03 +0200

    DONE: header, footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,10 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <div class="flex-grow-1"></div>
+    <v-app-bar app absolute color="white">
+      <v-btn id="logo" class="mx-2" text icon to="/">
+        <v-img
+          class="flex-grow-0"
+          :src="require('~/assets/images/artventures-logo.png')"
+          height="54px" width="84px"
+          contain
+        />
+      </v-btn>
+      <v-spacer />
       <v-toolbar-items>
-        <v-btn color="black--text" text to="/for-home"/>
-        <v-btn color="black--text" text to="/for-business"/>
+        <v-tabs optional color="black" height="100%">
+          <v-tab class="black--text" :ripple="false" to="/for-home">For Home</v-tab>
+          <v-tab class="black--text" :ripple="false" to="/for-business">For Business</v-tab>
+        </v-tabs>
       </v-toolbar-items>
     </v-app-bar>
     <v-content class="px-0 py-0">
@@ -26,6 +36,12 @@
       </v-footer>
   </v-app>
 </template>
+
+<style>
+#logo:active {
+  color: transparent;
+}
+</style>
 
 <static-query>
   query {
