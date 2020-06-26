558ccf8498e7bbb64f4a6727a0a742753cd21779
hathweels <12772651+hatwheels@users.noreply.github.com>
2020-06-27 02:33:20 +0300

    wpTransition: faq

--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -10,40 +10,21 @@
           <v-row justify="space-around" align="center">
             <v-col class="pb-0" cols="10">
               <div v-for="(qa, i) in $page.faq.edges[0].node.qa" :key="'faq-' + i">
-                <div class="pr-8" v-waypoint="{ active: true, callback: onFaqEl, options: elIsOpt }">
-                  <template v-if="faqEl">
-                    <transition name="slide" appear>
-                      <div>
-                        <div
-                          :class="getLang === 'gr' ? 'noto-18-600' : 'raleway-18-600'"
-                          v-html="qa.q[getLang]"
-                        >
-                      </div>
-                      <div
-                          :class="getLang === 'gr' ? 'noto-16-400' : 'raleway-16-400'"
-                          class="text-justify"
-                          v-html="qa.a[getLang]"
-                        />
-                        <br>
-                      </div>
-                    </transition>
-                  </template>
-                  <template v-else>
-                    <div>
-                      <div
-                        :class="getLang === 'gr' ? 'noto-18-600' : 'raleway-18-600'"
-                        v-html="qa.q[getLang]"
-                      >
-                    </div>
+                <wp-transition class="pr-8" :isActive="true" nm="surf" :dir="false">
+                  <div>
                     <div
-                        :class="getLang === 'gr' ? 'noto-16-400' : 'raleway-16-400'"
-                        class="text-justify"
-                        v-html="qa.a[getLang]"
-                      />
-                      <br>
-                    </div>
-                  </template>
-                </div>
+                      :class="getLang === 'gr' ? 'noto-18-600' : 'raleway-18-600'"
+                      v-html="qa.q[getLang]"
+                    >
+                  </div>
+                  <div
+                      :class="getLang === 'gr' ? 'noto-16-400' : 'raleway-16-400'"
+                      class="text-justify"
+                      v-html="qa.a[getLang]"
+                    />
+                    <br>
+                  </div>
+                </wp-transition>
               </div>
             </v-col>
           </v-row>
@@ -58,28 +39,14 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import WpTransition from '~/components/WpTransition.vue'
 
 export default {
-  data () {
-    return {
-      // Element CSS transitions
-      // intersection option for all
-      elIsOpt: {
-        threshold: [0],
-      },
-      faqEl: false,
-    }
-  },
   computed: {
     ...mapGetters(['getLang']),
   },
-  methods: {
-    /* Element CSS transitions */
-    onFaqEl ({ going, direction }) {
-      if (going === this.$waypointMap.GOING_IN) {
-        this.faqEl = true
-      }
-    },
+  components: {
+    WpTransition,
   },
   metaInfo () {
     return {
