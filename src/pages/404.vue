d574f7773a7708cc7f67c2c0fb7663bf6f937d0d
Kostas Markostamos <12772651+hatwheels@users.noreply.github.com>
2020-07-07 10:50:43 +0300

    Added custom 404 error page

---
+++ src/pages/404.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+
+      <v-content>
+        <v-container class="pa-0 background-color-fafafa" fluid>
+          <v-card color="rgba(250, 250, 250, 1)" :height="getHeight">
+            <v-row style="height: 100%" justify="center" align="center">
+              <v-col>
+                <p class="text-center" :class="getLang === 'gr' ? 'noto-30-700' : 'playfair-30-700'">404</p>
+                <p class="text-center" :class="getLang === 'gr' ? 'noto-16-400-1p6em' : 'raleway-16-400-1p6em'">{{ msg[getLang] }}</p>
+              </v-col>
+             </v-row>
+          </v-card>
+        </v-container>
+      </v-content>
+
+  </Layout>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  data () {
+    return {
+        msg: {
+            en: 'Oops, page not found.',
+            gr:  'Ωχ, η σελίδα δεν βρέθηκε.'
+        }
+    }
+  },
+  computed: {
+      ...mapGetters(['getLang']),
+    getHeight() {
+      return window.innerHeight;
+    }
+  }
+}
+</script>
