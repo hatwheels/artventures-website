34110f57ae6be4eb4c564cc89e2d87240adee400
Kostas Markostamos <12772651+hatwheels@users.noreply.github.com>
2020-08-11 13:46:41 +0300

    WIP: auth BE/FE 3

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 let webAuth = new auth0.WebAuth({
     domain: process.env.GRIDSOME_AUTH0_DOMAIN,
     clientID: process.env.GRIDSOME_AUTH0_CLIENT_ID,
-    redirectUri: 'http://localhost:8888/auth-callback',
+    redirectUri: 'http://localhost:8888/auth0cb',
     audience: 'https://' + process.env.GRIDSOME_AUTH0_DOMAIN + '/api/v2/',
     responseType: 'token id_token',
     scope: 'openid email profile'
