6d80fd146a5fc7bda7b6e1f0d24646d158281c77
Kostas Markostamos <12772651+hatwheels@users.noreply.github.com>
2020-09-07 10:34:49 +0300

    fix updateUserRole due to userRole localStorage

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -201,10 +201,6 @@ let auth = new Vue({
                     if (200 == res.status) {
                         if (!res.data || res.data.length == 0) {
                             resolve(null)
-                            // Assign default user
-                            // this.assignUserRole("user")
-                            // .then((roleObj) => resolve(roleObj))
-                            // .catch(err => reject(err))
                         } else {
                             this.userRole = res.data
                             resolve(res.data)
@@ -292,7 +288,6 @@ let auth = new Vue({
                 axios.post(process.env.GRIDSOME_SITE_URL + '/.netlify/functions/auth0-update-user-role',
                     {
                         user_id: this.user.sub,
-                        remove_role_id: this.userRole[0].name,
                         add_role_id: newRole,
                         token: process.env.GRIDSOME_AUTH0_MASTER_TOKEN,
                     },
