214204d242f68a8d65f22a4a941d19e25715041f
Kostas Markostamos <12772651+hatwheels@users.noreply.github.com>
2020-09-17 15:02:32 +0300

    mailchimp create for new logins/signups

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -295,6 +295,7 @@ export default {
               'LNAME': this.lastName,
               'SUBJECT': this.subject,
               'MESSAGE': this.message,
+              'ROLE': 'user'
           },
         };
         this.isLoading = true;
