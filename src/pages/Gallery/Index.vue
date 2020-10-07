d3274430dcd9f5d498ae3b38072cebe3a210b884
Kostas Markostamos <12772651+hatwheels@users.noreply.github.com>
2020-10-07 17:12:40 +0300

    fetched all artists with their auth ids

---
+++ src/pages/Gallery/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+    <v-main>
+
+    </v-main>
+  </Layout>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  created() {
+    this.$auth.getUsersInRole('artist')
+        .then(artists => {
+            this.artists = artists;
+            this.artists.forEach(artist => {
+              console.log(artist.user_id);
+            });
+        })
+        .catch(err => console.log(err))
+  },
+  data () {
+      return {
+        artists: undefined
+      }
+  },
+  metaInfo() {
+    return {
+      titleTemplate:
+        this.getLang === "gr"
+          ? "Γκαλερί — Artventures"
+          : "Gallery — Artventures",
+      meta: [{ name: "description", content: "Landing page" }],
+    };
+  },
+};
+</script>
